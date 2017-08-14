d04f6790db7389d046f6e0a1ed277337cb8ea922
Adrian Garza <adrianhgarza@gmail.com>
2017-08-13 21:57:46 -0700

    seekY

---
+++ Assets/Scripts/Monsters/SeekPlayerY.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekPlayerY : EnemyBehavior {
+
+	public override void Move() {
+		if (mainController.frozen) {
+			return;
+		}
+		//move towards the player
+		//first, get where they are
+		if (Mathf.Abs(playerObject.transform.position.y - this.transform.position.y) < seekThreshold) {
+			return;
+		}
+		int moveScale;
+		if (playerObject.transform.position.y > this.transform.position.y) {
+			moveScale = 1;
+			mainController.movingRight = true;
+		} else {
+			moveScale = -1;
+			mainController.movingRight = false;
+		}
+
+		if (Mathf.Abs(mainController.rb2d.velocity.y) < mainController.maxSpeed) {
+			mainController.rb2d.AddForce(new Vector2(mainController.rb2d.velocity.x, (mainController.moveSpeed * moveScale)));
+		}
+	}
+}
