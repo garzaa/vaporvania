194b2c7eac2b749208e2e74544693eb2cea896d5
Adrian Garza <adrianhgarza@gmail.com>
2017-08-20 13:02:21 -0700

    forgot to track the parry hitbox controller

---
+++ Assets/Scripts/Player/ParryHitboxController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParryHitboxController : MonoBehaviour {
+
+	public PlayerController pc;
+
+	BoxCollider2D parryHitbox;
+
+	void Start() {
+		pc = GameObject.Find("Player").GetComponent<PlayerController>();
+	}
+
+	public void OnTriggerEnter2D(Collider2D boneHurtingCollider) {
+		if (pc.parrying && boneHurtingCollider.tag == "enemyHitbox") {
+			if (boneHurtingCollider.GetComponent<Projectile>()) {
+                boneHurtingCollider.GetComponent<Projectile>().Reflect();
+            } else if (boneHurtingCollider.transform.parent.GetComponent<Enemy>()) {
+                if (boneHurtingCollider.tag == "enemyHitbox") {
+                    pc.Riposte(boneHurtingCollider.transform.parent.gameObject);
+                }
+            }
+		}
+	}
+}
