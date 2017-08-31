1d8cb83afbb634d8c5fc411c19f325c199a1c7ef
Adrian Garza <adrianhgarza@gmail.com>
2017-08-30 21:35:03 -0700

    horizontal repeat for moving right

---
+++ Assets/Scripts/Global/HorizontalRepeater.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// usage: attach this to one object, have another one
+/// named the same thing but with "2" at the end, only
+/// the position of the first one matters
+/// 
+/// this script takes two gameobjects and places them
+/// side by side. when one moves off the screen it's
+/// put in front of the other so they repeat
+/// 
+/// backgroundAnchor is what to use for the screen borders
+/// 
+/// </summary>
+
+public class HorizontalRepeater : MonoBehaviour {
+
+	//optional move speed, leave at 0 for parallax-enabled objectss
+    public float speed = 0;
+
+    //this overlaps the two sprites so there isn't any flickering 
+    //if they move slowly, .002 is usually fine
+    public float overlap = .002f;
+
+    public float horizMin;
+    public GameObject other;
+    SpriteRenderer sr;
+
+	GameObject mainCamera;
+	float cameraSize;
+
+	// Use this for initialization
+    void Start () {
+
+        //if (other == null) other = GameObject.Find(name + "2");
+      
+        //get the left edge of the camera
+		mainCamera = GameObject.FindGameObjectsWithTag("MainCamera")[0];
+		cameraSize = mainCamera.GetComponent<Camera>().orthographicSize;
+        horizMin = mainCamera.transform.position.x - cameraSize;
+
+        //pin the second sprite to the right edge of the first sprite
+        float length = other.GetComponent<SpriteRenderer>().bounds.extents.x;
+        float newX = other.transform.position.x;
+
+        sr = other.GetComponent<SpriteRenderer>();
+        //other.transform.position = new Vector3(2 * (temp.bounds.max.x - temp.bounds.min.x), this.transform.position.y, this.transform.position.z);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizMin = mainCamera.transform.position.x - cameraSize;
+
+	    if (speed != 0)
+        {
+            gameObject.transform.Translate(-speed, 0, 0);
+            other.transform.Translate(-speed, 0, 0);
+
+        }
+
+		//if one is off the screen move it up
+		if (gameObject.GetComponent<SpriteRenderer>().bounds.max.x < horizMin)
+		{
+			Debug.Log("Moving up");
+			sr = gameObject.GetComponent<SpriteRenderer>();
+			float length = sr.bounds.max.x - sr.bounds.min.x;
+			gameObject.transform.Translate(2 * length - overlap, 0, 0);
+		}
+
+		if (other.GetComponent<SpriteRenderer>().bounds.max.x < horizMin)
+		{
+			Debug.Log("Moving up");
+			sr = other.GetComponent<SpriteRenderer>();
+			float length = sr.bounds.max.x - sr.bounds.min.x;
+			other.transform.Translate(2 * length - overlap, 0, 0);
+		}
+    }
+}
---
+++ Assets/Scripts/Global/VerticalParallax.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//does what it says on the tin
+public class VerticalParallax : MonoBehaviour {
+
+	//movement ratio to that of the player, 0 is stationary
+    public float ratio;
+
+    GameObject player;
+    float playerOrigY;
+    float origY;
+
+    //new stuff
+    float prevY;
+    float currY;
+
+    void Start () {
+        player = GameObject.Find("Player");
+        currY = player.transform.position.y;
+        prevY = player.transform.position.y;
+	}
+	
+	void Update () {
+        if (ratio != 0)
+        {
+
+            currY = player.transform.position.y;
+
+            this.transform.Translate(new Vector2(0, ratio * (currY - prevY)));
+
+            prevY = player.transform.position.y;
+
+        }
+	}
+}
