ef33f0e531424f9c18a1b4d2bc9ffac6f27091d8
Adrian Garza <adrianhgarza@gmail.com>
2017-09-01 14:29:30 -0700

    Water drop, stalagmites/stalactites

---
+++ Assets/Scripts/Global/PrefabSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour {
+
+	public float interval = .2f;
+
+	public GameObject toSpawn;
+
+	void Start() {
+		StartCoroutine(Spawn());
+	}
+
+	IEnumerator Spawn() {
+		yield return new WaitForSeconds(interval);
+		Instantiate(toSpawn, this.transform.position, Quaternion.identity);
+		StartCoroutine(Spawn());
+	}
+}
---
+++ Assets/Scripts/Misc/WaterDropController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDropController : SelfDestruct {
+
+	Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator>();
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		anim.SetTrigger("splash");
+	}
+
+}
