dd8d086ed11ce717935db602d9f2d7e2533aaad8
Adrian Garza <adrianhgarza@gmail.com>
2017-12-30 17:35:06 -0800

    tweaked dialogue, changed jumping sludge animation state

--- Assets/Scripts/NPCs/TrashWorld/Steven.cs
+++ Assets/Scripts/NPCs/TrashWorld/Steven.cs
@@ -7,16 +7,15 @@ public class Steven : NPC {
 	public override void CreateDialogue() {
 		List<string> testConvo = new List<string>();
 		testConvo.Add("Nice sword!\nIt really ties the look together.");
-		testConvo.Add("Really, though, I expected more.");
 		testConvo.Add("!...");
 		testConvo.Add("Suit yourself.");
 		
 		convos.Add(testConvo);
 
 		testConvo = new List<string>();
-		testConvo.Add("!So what's your deal?");
-		testConvo.Add("I just like it down here. What's your deal?");
-		testConvo.Add("!Trying to get back to the surface.");
+		testConvo.Add("What's your deal, you get spit out by a Paradise lab?");
+		testConvo.Add("Looks like it, from the way you're dressed.\nMinus the big sword.");
+		testConvo.Add("!Just trying to get back to the surface.");
 		testConvo.Add("Good luck with that.");
 		convos.Add(testConvo);
 
