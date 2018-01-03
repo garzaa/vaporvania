5c0f30bb40c5a88752aedd178306c34e827f9173
Adrian Garza <adrianhgarza@gmail.com>
2018-01-02 23:16:16 -0800

    breakable script

---
+++ Assets/Scripts/Global/Breakable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//extends SelfDestruct so the animation clip can call it
+public class Breakable : SelfDestruct {
+
+	Animator anim = null;
+	GameObject wallCollider = null;
+	bool broken = false;
+
+	void Start() {
+		if (GetComponent<Animator>() != null) {
+			anim = GetComponent<Animator>();
+		}
+		foreach (Transform child in transform) {
+			if (child.GetComponent<BoxCollider2D>()) {
+				wallCollider = child.gameObject;
+				return;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "playerAttack" && !broken) {
+			Break();
+		}
+	}
+
+	public void Break() {
+		broken = true;
+		if (wallCollider != null) {
+			Destroy(wallCollider);
+		}
+		
+		if (anim != null) {
+			anim.SetTrigger("break");
+		}
+	}
+
+}
