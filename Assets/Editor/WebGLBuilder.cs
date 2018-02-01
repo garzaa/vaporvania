21b7b101a06201277efeddd19b044e018e78200d
Adrian Garza <adrianhgarza@gmail.com>
2018-02-01 17:37:16 -0500

    auto builder, jab2 animation

---
+++ Assets/Editor/WebGLBuilder.cs
@@ -0,0 +1,12 @@
+﻿//place this script in the Editor folder within Assets.
+ using UnityEditor;
+  
+ //to be used on the command line:
+ //$ Unity -quit -batchmode -executeMethod WebGLBuilder.build
+  
+ class WebGLBuilder {
+	static void build() {
+		string[] scenes = {"Assets/main.unity"};
+		BuildPipeline.BuildPlayer(scenes, "WebGL-Dist", BuildTarget.WebGL, BuildOptions.InstallInBuildFolder);
+	}
+ }
---
+++ Assets/Editor/build.bat
@@ -0,0 +1,1 @@
+Unity -quit -batchmode -executeMethod WebGLBuilder.build
