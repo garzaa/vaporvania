457c72b420edf465c7f2452f9b70e8bbdfa5ce88
Adrian Garza <adrianhgarza@gmail.com>
2018-04-04 01:35:02 -0400

    basics for better UI scripts

---
+++ Assets/Scripts/Global/UIComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// inherited by other inventory components like health, dialogue, letterboxes, etc
+public class UIComponent : MonoBehaviour {
+
+	public void Show() {
+		this.gameObject.SetActive(true);
+	}
+
+	public void Hide() {
+		this.gameObject.SetActive(false);
+	}
+	
+}
